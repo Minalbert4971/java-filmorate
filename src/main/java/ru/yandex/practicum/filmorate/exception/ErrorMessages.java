c44fd823fe8c8c044d74b0b09aea926c957f3481
Альберт Миннуллин <minnullin.albert@mail.ru>
2024-11-24 12:44:50 +0500

    Выполнил ТЗ №11: написал контроллеры, сервисы, хранилища и обработку ошибок.

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorMessages.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorMessages.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class ErrorMessages {
-
     public static final String FILM_NOT_FOUND = "There is no film with such id: ";
 
     public static final String USER_NOT_FOUND = "There is no user with such id: ";
