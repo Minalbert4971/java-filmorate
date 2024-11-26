090e41c7301ab112f881023c1902863e2f4bf04d
Альберт Миннуллин <minnullin.albert@mail.ru>
2024-11-26 11:49:56 +0500

    Redesigned the ControllerExceptionHandler class due to the addition of a new ErrorResponse class.

--- src/main/java/ru/yandex/practicum/filmorate/controller/ControllerExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ControllerExceptionHandler.java
@@ -2,48 +2,35 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler {
 
-    private static final String ERROR = "error";
-
-    private static final String MESSAGE = "message";
-
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleNotFoundException(NotFoundException ex) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final NotFoundException ex) {
         log.debug("Not found error: {}", ex.getMessage());
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put(ERROR, "Not Found");
-        errorResponse.put(MESSAGE, ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        return new ErrorResponse("ERROR", "Not found");
     }
 
     @ExceptionHandler({ValidationException.class, MethodArgumentNotValidException.class})
-    public ResponseEntity<Map<String, String>> handleValidationException(ValidationException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException ex) {
         log.debug("Validation error: {}", ex.getMessage());
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put(ERROR, "Validation doesn't pass");
-        errorResponse.put(MESSAGE, ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return new ErrorResponse("ERROR", "Validation doesn't pass");
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleGeneralException(Exception ex) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleGeneralException(Exception ex) {
         log.debug("Unexpected error: {}", ex.getMessage());
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put(ERROR, "Unexpected error");
-        errorResponse.put(MESSAGE, ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ErrorResponse("ERROR", "Unexpected error");
     }
 }
